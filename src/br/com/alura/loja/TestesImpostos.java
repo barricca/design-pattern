0301f322a1f74917673f1c41c526f71d0f17e2ff
barricca <williamsgomess@outlook.com>
2021-12-08 01:16:33 -0300

    Strategy

---
+++ src/br/com/alura/loja/TestesImpostos.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesImpostos {
+
+    public static void main(String[] args) {
+        var orcamento = new Orcamento(new BigDecimal("100"));
+        var calculadoraDeImpostos = new CalculadoraDeImpostos();
+
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ISS()));
+
+    }
+}
