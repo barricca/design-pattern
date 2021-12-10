57c5655edb8e59e1bf73fa4972a2af2b3a1bb9a5
barricca <barricca.gomes@gmail.com>
2021-12-10 00:45:33 -0300

    State

---
+++ README.md
@@ -0,0 +1,6 @@
+#Design Patterns
+
+- Strategy
+- Chain of Responsibility
+- Template Method
+- State
---
+++ src/br/com/alura/loja/orcamento/situacao/Reprovado.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class Reprovado extends SituacaoOrcamento {
+
+    public void reprovar(Orcamento orcamento) {
+        orcamento.setSituacao(new Reprovado());
+    }
+}
